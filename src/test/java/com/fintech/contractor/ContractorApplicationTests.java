bcac27b4767fa8b8a9bad6603a667e5e4c3a8363
1dlvb <matushkin-2017@mail.ru>
2024-07-03 21:01:07 +0500

    test: basic tests

---
+++ src/test/java/com/fintech/contractor/ContractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.contractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
