2ce91d34800e97a34ad288874b770948822e4c01
1dlvb <matushkin-2017@mail.ru>
2024-07-03 21:00:48 +0500

    feat: register Auditor as a bean

---
+++ src/main/java/com/fintech/contractor/ContractorApplication.java
@@ -0,0 +1,33 @@
+package com.fintech.contractor;
+
+import com.fintech.contractor.auditor.AuditorAware;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class ContractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractorApplication.class, args);
+    }
+
+    @Bean
+    public org.springframework.data.domain.AuditorAware<String> auditorProvider() {
+        return new AuditorAware();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+}
