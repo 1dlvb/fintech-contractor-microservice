1a791118d1b8351310ad570e6129c9039add9e1d
1dlvb <matushkin-2017@mail.ru>
2024-07-05 15:56:00 +0500

    feat: add NotActiveException for cases when object wasn't found by ID

---
+++ src/main/java/com/fintech/contractor/exception/NotActiveException.java
@@ -0,0 +1,9 @@
+package com.fintech.contractor.exception;
+
+public class NotActiveException extends Throwable {
+
+    public NotActiveException(String message) {
+        super(message);
+    }
+
+}
