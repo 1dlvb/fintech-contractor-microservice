82716feb661cf68b79d54b0715f421a6dceb8bf2
1dlvb <matushkin-2017@mail.ru>
2024-07-06 11:13:46 +0500

    refactor: use streams instead of list of BiFunctions

--- src/main/java/com/fintech/contractor/repository/specification/ContractorSpecification.java
+++ src/main/java/com/fintech/contractor/repository/specification/ContractorSpecification.java
@@ -7,56 +7,56 @@ import com.fintech.contractor.model.OrgForm;
 import com.fintech.contractor.payload.SearchContractorPayload;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class ContractorSpecification {
 
     private ContractorSpecification() {}
 
     public static Specification<Contractor> findContractorBySpecifications(SearchContractorPayload payload) {
-        List<Predicate> filterConditions = new ArrayList<>();
         return (root, query, criteriaBuilder) -> {
-            List<BiFunction<CriteriaBuilder, Root<Contractor>, Predicate>> fieldPredicates = List.of(
-                    (cb, r) -> createEqualPredicate(cb, r, "id", payload.id()),
-                    (cb, r) -> createEqualPredicate(cb, r, "parent.id", payload.parentId()), // Note: Changed to "parent.id"
-                    (cb, r) -> createLikePredicate(cb, r, "name", payload.name()),
-                    (cb, r) -> createLikePredicate(cb, r, "nameFull", payload.nameFull()),
-                    (cb, r) -> createLikePredicate(cb, r, "inn", payload.inn()),
-                    (cb, r) -> createLikePredicate(cb, r, "ogrn", payload.ogrn())
+            Stream<Predicate> predicateStream = Stream.of(
+                    criteriaBuilder.isTrue(root.get("isActive")),
+                    createEqualPredicate(criteriaBuilder, root, "id", payload.id()),
+                    createEqualPredicate(criteriaBuilder, root, "parentId", payload.parentId()),
+                    createLikePredicate(criteriaBuilder, root, "name", payload.name()),
+                    createLikePredicate(criteriaBuilder, root, "nameFull", payload.nameFull()),
+                    createLikePredicate(criteriaBuilder, root, "inn", payload.inn()),
+                    createLikePredicate(criteriaBuilder, root, "ogrn", payload.ogrn())
             );
 
-            fieldPredicates.stream()
-                    .map(predicate -> predicate.apply(criteriaBuilder, root))
+            List<Predicate> predicates = predicateStream
                     .filter(Objects::nonNull)
-                    .forEach(filterConditions::add);
+                    .collect(Collectors.toList());
 
-            if (payload.countryName() != null) {
-                Join<Contractor, Country> join = root.join("country");
-                filterConditions.add(criteriaBuilder.like(join.get("name"),
-                        enhanceWithWildcatMatching(payload.countryName())));
+
+            if (payload.country() != null) {
+                Join<Contractor, Country> countryJoin = root.join("country", JoinType.LEFT);
+                predicates.add(criteriaBuilder.like(countryJoin.get("name"),
+                        enhanceWithWildcatMatching(payload.country())));
             }
 
             if (payload.industry() != null) {
-                Join<Contractor, Industry> join = root.join("industry");
-                filterConditions.add(criteriaBuilder.equal(join.get("id"), payload.industry().getId()));
-                filterConditions.add(criteriaBuilder.equal(join.get("name"),
-                        enhanceWithWildcatMatching(payload.industry().getName())));
+                Join<Contractor, Industry> industryJoin = root.join("industry");
+                predicates.add(criteriaBuilder.equal(industryJoin.get("id"), payload.industry().getId()));
+                predicates.add(criteriaBuilder.equal(industryJoin.get("name"), payload.industry().getId()));
             }
 
-            if (payload.orgFormName() != null) {
-                Join<Contractor, OrgForm> join = root.join("orgForm");
-                filterConditions.add(criteriaBuilder.like(join.get("name"),
-                        enhanceWithWildcatMatching(payload.orgFormName())));
+            if (payload.orgForm() != null) {
+                Join<Contractor, OrgForm> orgFormJoin = root.join("orgForm");
+                predicates.add(criteriaBuilder.like(orgFormJoin.get("name"),
+                        enhanceWithWildcatMatching(payload.orgForm())));
             }
 
-            return criteriaBuilder.and(filterConditions.toArray(new Predicate[0]));
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
     }
 
