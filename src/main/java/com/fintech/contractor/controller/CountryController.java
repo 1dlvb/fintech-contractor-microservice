a1dd42696a462a8c735e8012ac32e01e07058410
1dlvb <matushkin-2017@mail.ru>
2024-08-01 20:14:49 +0500

    fix: typo in @ApiResponse

--- src/main/java/com/fintech/contractor/controller/CountryController.java
+++ src/main/java/com/fintech/contractor/controller/CountryController.java
@@ -107,8 +107,8 @@ public class CountryController {
      */
     @Operation(summary = "Delete country by ID", description = "Deletes country by ID.")
     @ApiResponses({
-            @ApiResponse(responseCode = "200",
-                    description = "Country found",
+            @ApiResponse(responseCode = "204",
+                    description = "Country deleted",
                     content = @Content(schema = @Schema(implementation = CountryDTO.class))),
             @ApiResponse(responseCode = "404",
                     description = "Country not found")
